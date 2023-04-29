40624c8d77a7d5cf412d689e765dfbe7f69c55b4
DtlipKbf <lizabeta.li.p@gmail.com>
2023-04-29 19:37:49 +0300

    добавили высокоуровневый класс группа реализовали инкапсуляцию на уровне контейнерного класса

--- lessonOOPExampleProject/manager.cpp
+++ lessonOOPExampleProject/manager.cpp
@@ -1,1 +1,12 @@
 #include "manager.h"
+
+float Manager::calcStAvgAge(Group group) {
+	float avg = 0;
+
+	for (int i = 0; i < group.getSize(); i++) {
+		// Student st = group.getStudent(i)  st.getAge()
+		avg += group.getStudent(i).getAge();
+	}
+	return avg / group.getSize();
+}
+
--- lessonOOPExampleProject/manager.h
+++ lessonOOPExampleProject/manager.h
@@ -1,9 +1,9 @@
-#include "student.h"
+#include "group.h"
 
 class Manager {
 public:
-	float calcStAvgAge(Student* list, int size);
-	float calcStAvgMark(Student* list, int size);
-	Student findBestSt(Student* list, int size);
-	Student findWorstSt(Student* list, int size);
-}
+	float calcStAvgAge(Group group);
+	float calcStAvgMark(Group group);
+	Student findBestSt(Group group);
+	Student findWorstSt(Group group);
+};
