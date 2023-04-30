741cdf933516ea5bab5a5d9183f9339669430645
DtlipKbf <lizabeta.li.p@gmail.com>
2023-04-30 18:49:02 +0300

    добавление реализации функций

--- lessonOOPExampleProject/manager.cpp
+++ lessonOOPExampleProject/manager.cpp
@@ -10,3 +10,35 @@ float Manager::calcStAvgAge(Group group) {
 	return avg / group.getSize();
 }
 
+float Manager::calcStAvgMark(Group group){
+	float avg = 0;
+
+	for (int i = 0; i < group.getSize(); i++) {
+		avg += group.getStudent(i).getAvg_mark();
+	}
+
+	return avg / group.getSize();
+}
+
+Student Manager::findBestSt(Group group){
+	Student st = group.getStudent(0);
+
+	for (int i = 1; i < group.getSize(); i++) {
+		if (st.getAvg_mark() < group.getStudent(i).getAvg_mark()) {
+			st = group.getStudent(i);
+		}
+	}
+
+	return st;
+}
+Student Manager::findWorstSt(Group group){
+	Student st = group.getStudent(0);
+
+	for (int i = 1; i > group.getSize(); i++) {
+		if (st.getAvg_mark() < group.getStudent(i).getAvg_mark()) {
+			st = group.getStudent(i);
+		}
+	}
+
+	return st;
+}
