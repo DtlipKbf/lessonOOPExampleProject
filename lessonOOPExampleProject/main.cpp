0384cf42141d2a3549d6b3beb02726ae84c4b23b
DtlipKbf <lizabeta.li.p@gmail.com>
2023-05-02 18:40:32 +0300

    создание группы

--- lessonOOPExampleProject/main.cpp
+++ lessonOOPExampleProject/main.cpp
@@ -1,6 +1,18 @@
-#include "student.h"
+#include "manager.h"
 
 int main() {
-	
+	Student st1("ivan", "ivanov", 15, 8.3);
+	Student st2("harry", "poter", 16, 9.2);
+	Student st3("peter", "pen", 14, 7.6);
+
+	Group group;
+	group.add(st1);
+	group.add(st2);
+	group.add(st3);
+
+	Manager manager;
+
+	Student s = manager.findBestSt(group);
+	cout << "best st: " << s.convert();
 	return 0;
 }
